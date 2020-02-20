194c10342bb3a109ba752b9c29ed2d92084315bf
Brian Hambleton <bhambl3t0n1@gmail.com>
2020-02-20 06:14:31 -0800

    Basic implementation of mandelbrot set

---
+++ src/app.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    console.log(this.refs.canvas.height);
+    this.mandelbrot();
+  }
+
+  drawPoint = (x, y, color) => {
+    const ctx = this.refs.canvas.getContext("2d");
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, 1, 1);
+  };
+
+  inMandelbrot = (x, y) => {
+    let iteration = 0;
+    let max = 1000;
+    let initX = 0;
+    let initY = 0;
+    let real = ((x - this.refs.canvas.width / 2) * 4) / this.refs.canvas.width;
+    let imaginary =
+      ((y - this.refs.canvas.height / 2) * 4) / this.refs.canvas.width;
+
+    while (Math.pow(initX, 2) + Math.pow(initY, 2) <= 4 && iteration < max) {
+      let newX = Math.pow(initX, 2) - Math.pow(initY, 2) + real;
+      initY = 2 * initY * initX + imaginary;
+      initX = newX;
+      iteration++;
+    }
+
+    if (iteration < max) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  mandelbrot = () => {
+    for (let i = 0; i < this.refs.canvas.height; i++) {
+      for (let j = 0; j < this.refs.canvas.width; j++) {
+        //convert x and y to be out of 2
+        //throw x and y in function that determines if x and y are in mandelbrot
+        if (this.inMandelbrot(i, j)) {
+          this.drawPoint(i, j);
+        }
+      }
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <div id="header">Mandelbrot</div>
+        <div className="center">
+          <canvas ref="canvas" height={800} width={800}></canvas>
+        </div>
+      </>
+    );
+  }
+}
